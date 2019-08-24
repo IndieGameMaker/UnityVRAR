ad0b35ad41c587c7a881fcc8db2492db2ae51cf8
Jaehyun Lee <myleje@gmail.com>
2019-08-24 11:49:39 +0900

    .

--- ShadowGunVR/Assets/02.Scripts/ItemBox.cs
+++ ShadowGunVR/Assets/02.Scripts/ItemBox.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemBox : MonoBehaviour
+public class ItemBox : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public void OnLookAt(bool isLooked)
     {
-        MoveLookAt.isStopped = isLooked;
+        //MoveLookAt.isStopped = isLooked;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveLookAt.isStopped = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveLookAt.isStopped = false;
     }
 }
