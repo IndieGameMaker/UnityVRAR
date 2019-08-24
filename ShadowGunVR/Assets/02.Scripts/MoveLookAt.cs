aad39575499fbe798fd9452207262365ae5b007f
Jaehyun Lee <myleje@gmail.com>
2019-08-24 11:17:02 +0900

    .

---
+++ ShadowGunVR/Assets/02.Scripts/MoveLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLookAt : MonoBehaviour
+{
+    private CharacterController cc;
+    private Transform camTr;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        camTr = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        cc.SimpleMove(camTr.forward * speed);
+    }
+}
