cea6bd77eacbf6784bc682459693122e52897ab4
Jaehyun Lee <myleje@gmail.com>
2019-08-24 13:33:54 +0900

    .

--- ShadowGunVR/Assets/02.Scripts/EyeCast.cs
+++ ShadowGunVR/Assets/02.Scripts/EyeCast.cs
@@ -17,5 +17,13 @@ public class EyeCast : MonoBehaviour
     {
         ray = new Ray(camTr.position, camTr.forward);
         Debug.DrawRay(ray.origin, ray.direction * 20.0f, Color.green);
+        if (Physics.Raycast(ray, out hit, 20.0f, 1<<8))
+        {
+            hit.collider.gameObject.GetComponent<ItemBox>().OnLookAt(true);
+        }
+        else
+        {
+            hit.collider.gameObject.GetComponent<ItemBox>().OnLookAt(false);
+        }
     }
 }
