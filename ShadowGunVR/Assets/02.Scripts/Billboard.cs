72963b3118f095b3cfd9f63ae976e3d90fb71244
Jaehyun Lee <myleje@gmail.com>
2019-08-24 14:59:58 +0900

    .

---
+++ ShadowGunVR/Assets/02.Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform camTr;
+    private Transform tr;
+    
+    void Start()
+    {
+        camTr = Camera.main.transform;
+        tr    = transform;    
+    }
+
+    void LateUpdate()
+    {
+        tr.LookAt(camTr.position);
+    }
+}
