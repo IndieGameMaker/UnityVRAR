6c5e6a9232d04fa2a005960c108ae5142e246578
Jaehyun Lee <myleje@gmail.com>
2019-08-31 13:17:25 +0900

    .

--- ShadowGunVR/Assets/02.Scripts/Controller.cs
+++ ShadowGunVR/Assets/02.Scripts/Controller.cs
@@ -11,12 +11,15 @@ public class Controller : MonoBehaviour
 
     void Update()
     {
+        //트리거 버튼 클릭 여부
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
             Debug.Log("Trigger Button Click");
         }
+        //터치패드 터치 여부
         if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
         {
+            //터치패드의 좌표값
             Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
             Debug.LogFormat("Touch position x={0}, y={1}",pos.x, pos.y);
         }
