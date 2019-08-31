549eea4c3b8dc797a5bcd361a167e0aa6647a18a
Jaehyun Lee <myleje@gmail.com>
2019-08-31 11:54:27 +0900

    ,

--- ShadowGunVR/Assets/02.Scripts/LaserPointer.cs
+++ ShadowGunVR/Assets/02.Scripts/LaserPointer.cs
@@ -6,14 +6,20 @@ public class LaserPointer : MonoBehaviour
 {
     public Transform controller;
     public LineRenderer laser;
+    public Transform pointerPrefab;
+
     //레이의 거리
     public float range = 10.0f;
 
     private Ray ray;
     private RaycastHit hit;
+    private Transform pointer;
 
     void Start()
     {
+        //포인터 생성
+        pointer = Instantiate(pointerPrefab);
+        //라인렌더러의 길이 설정
         laser.SetPosition(1, new Vector3(0, 0, range));
     }
 
@@ -24,10 +30,15 @@ public class LaserPointer : MonoBehaviour
         if (Physics.Raycast(ray, out hit, range))
         {
             laser.SetPosition(1, new Vector3(0,0,hit.distance));
+            pointer.position = hit.point;
+            pointer.rotation = Quaternion.LookRotation(hit.normal);
         }
         else
         {
             laser.SetPosition(1, new Vector3(0,0,range));
+            pointer.position = controller.position + (controller.forward * range);
+            pointer.LookAt(controller.position);
+            //pointer.rotation = Quaternion.LookRotation(pointer.position - controller.position);
         }
     }
 }
