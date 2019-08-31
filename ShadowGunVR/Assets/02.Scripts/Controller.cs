79665a50a6203f31be025b6c41ef85977f80944c
Jaehyun Lee <myleje@gmail.com>
2019-08-31 13:13:57 +0900

    .

---
+++ ShadowGunVR/Assets/02.Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger Button Click");
+        }
+    }
+}
