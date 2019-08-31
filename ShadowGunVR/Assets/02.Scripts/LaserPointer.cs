356c58b03932ba39ffad0a07a2ba5c84162fd0a1
Jaehyun Lee <myleje@gmail.com>
2019-08-31 11:41:42 +0900

    .

--- ShadowGunVR/Assets/02.Scripts/LaserPointer.cs
+++ ShadowGunVR/Assets/02.Scripts/LaserPointer.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class LaserPointer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform controller;
+    public LineRenderer laser;
+    //레이의 거리
+    public float range = 10.0f;
+
+    private Ray ray;
+    private RaycastHit hit;
+
     void Start()
     {
-        
+        laser.SetPosition(1, new Vector3(0, 0, range));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //레이를 생성
+        ray = new Ray(controller.position, controller.forward);
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            laser.SetPosition(1, new Vector3(0,0,hit.distance));
+        }
+        else
+        {
+            laser.SetPosition(1, new Vector3(0,0,range));
+        }
     }
 }
