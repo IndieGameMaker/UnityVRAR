9f68373ca61bf2aa0c36962831958792a48e1b2d
Jaehyun Lee <myleje@gmail.com>
2019-08-31 13:43:30 +0900

    .

--- ShadowGunVR/Assets/02.Scripts/Controller.cs
+++ ShadowGunVR/Assets/02.Scripts/Controller.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Transform camTr;
+    public CharacterController cc;
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();    
     }
 
     void Update()
@@ -17,11 +20,19 @@ public class Controller : MonoBehaviour
             Debug.Log("Trigger Button Click");
         }
         //터치패드 터치 여부
-        if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
+        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
         {
             //터치패드의 좌표값
             Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
             Debug.LogFormat("Touch position x={0}, y={1}",pos.x, pos.y);
+            if (pos.y >= 0.2f)
+            {
+                cc.SimpleMove(camTr.forward * 2.0f);
+            }
+            if (pos.y <= -0.2f)
+            {
+                cc.SimpleMove(-camTr.forward * 2.0f);
+            }
         }
     }
 }
